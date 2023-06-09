c087a372fceff08bc4b6eccbc4bdfddb3f9dc419
Lolikana <thomas.guiart@gmail.com>
2023-06-09 13:34:06 +0900

    fix __tests__ considered to be a test since  vitest v0.32.0

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,8 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./__tests__/setup.ts']
+    setupFiles: './__tests__/setup.ts',
+    exclude: ['node_modules', './__tests__/setup.ts']
   },
   server: {
     port: 3000
