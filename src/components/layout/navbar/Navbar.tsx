896dca513bd0fe3f9bff44b4f79ad2bf19b6c1f0
Lolikana <thomas.guiart@gmail.com>
2023-06-13 11:09:22 +0900

    navbar front done

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -11,6 +11,15 @@ const Navbar = () => {
     setisHidden(prevState => !prevState);
   };
 
+  let resizeTimer: ReturnType<typeof setTimeout>;
+  window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+  });
+
   return (
     <>
       <Burger setNavMenu={toogleNavMenu} />
