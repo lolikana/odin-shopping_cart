15cd5ee6fb556d938530f8121edfa9caa8eada6c
Lolikana <thomas.guiart@gmail.com>
2023-06-14 14:32:53 +0900

    custom selectors

--- store/features/cartSlice.ts
+++ store/features/cartSlice.ts
@@ -1,5 +1,7 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ICartItem, IProduct } from 'utils/types';
+
+import { RootState } from '../store';
 
 export interface ICartState {
   cartItems: ICartItem[];
@@ -13,8 +15,11 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    // Find the cart item with the matching product ID
     increment: (state, action: PayloadAction<IProduct>) => {
-      const item = state.cartItems.find(el => el.product.id === action.payload.id);
+      const item = state.cartItems.find(
+        (el: ICartItem) => el.product.id === action.payload.id
+      );
 
       if (item) item.qty++;
       else {
@@ -25,12 +30,16 @@ export const cartSlice = createSlice({
       }
     },
 
+    // Reducer for decrementing the quantity of a product in the cart
     decrement: (state, action: PayloadAction<IProduct>) => {
-      const item = state.cartItems.find(el => el.product.id === action.payload.id);
+      const item = state.cartItems.find(
+        (el: ICartItem) => el.product.id === action.payload.id
+      );
 
       if (item) {
         item.qty--;
         if (item.qty === 0) {
+          // If the quantity reaches 0, remove the item from the cart
           state.cartItems = state.cartItems.filter(
             el => el.product.id !== action.payload.id
           );
@@ -39,6 +48,27 @@ export const cartSlice = createSlice({
     }
   }
 });
+
+const cartItems = (state: RootState) => state.cart.cartItems;
+
+export const totalCartItemSelector = createSelector(
+  [cartItems],
+  (cartItems: ICartItem[]) =>
+    cartItems.reduce((total: number, curr: ICartItem) => (total += curr.qty), 0)
+);
+
+export const totalPriceSelector = createSelector([cartItems], (cartItems: ICartItem[]) =>
+  cartItems.reduce(
+    (total: number, curr: ICartItem) => (total += curr.qty * curr.product.price),
+    0
+  )
+);
+
+export const productQtySelector = createSelector(
+  //allow passing additional arguments to the selector, here productId
+  [cartItems, (_cartItems, productId: number) => productId],
+  (cartItems, productId: number) => cartItems.find(el => el.product.id === productId)?.qty
+);
 
 export const { increment, decrement } = cartSlice.actions;
 
